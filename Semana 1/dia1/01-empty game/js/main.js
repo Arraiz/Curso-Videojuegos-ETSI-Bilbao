38abfb5e4dc09d9d755c4fd8dc24b85cff4d3491
arraiz <mikeldiezgarcia@gmail.com>
2018-07-10 23:59:02 +0200

    added flappy bird example

--- Semana 1/dia1/01-empty game/js/main.js
+++ Semana 1/dia1/01-empty game/js/main.js
@@ -1,22 +1,56 @@
+var game = new Phaser.Game(320, 640, Phaser.AUTO, 'flappy', { preload: preload, create: create,update:update });
 
-var GameState = {
 
-  preload: function() {
-    this.load.image('background', 'assets/images/background.png');
-    
-    
-  },
+var fondo;
+var pajaro;
+var spaceKey;
+function preload() {
+
+//CARGAMOS NUESTRAS IMAGENES
+  game.load.image('fondo', 'assets/images/fondo.png');
+  game.load.image('pajaro', 'assets/images/Flappy_Bird.png');
+
+}
+
+function create() {
+
+//CREAMOS NUESTAS IMAGENES
+  fondo = game.add.sprite(0, 0, 'fondo');
+  pajaro = game.add.sprite(0, 0, 'pajaro');
+  //ESCALAMOS EL PAJARO
+  pajaro.anchor.setTo(0.5);
+  pajaro.scale.setTo(0.5);
+
+  //MOVEMOS EL PAJARO
+  pajaro.position.setTo(100,70);
+
+
+  //TECLA SPACIO
+  spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+}
+
+
+function update(){
+
+
+if(spaceKey.isDown==true){
+  pajaroSube();
+}else{
+  pajaroCae()
+}
+
  
-  create: function() {
-  },
-
-  update: function() {
-  },
-  
-
-};
 
 
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+}
+
+
+function pajaroCae(){
+  pajaro.position.y=pajaro.position.y+5;
+}
+
+function pajaroSube(){
+  pajaro.position.y=pajaro.position.y-20;
+}
+
