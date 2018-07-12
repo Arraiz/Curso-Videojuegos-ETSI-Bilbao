a028c6029ef39d5657d36b735edcd34dc4bacbbd
arraiz <mikeldiezgarcia@gmail.com>
2018-07-13 00:14:36 +0200

    added gravity

--- Semana 1/dia1/Flappy_Bird2/js/main.js
+++ Semana 1/dia1/Flappy_Bird2/js/main.js
@@ -6,12 +6,15 @@ var pajaro;
 var spaceKey;
 var pajaroVolando;
 var tuberia;
-
+var tuberiaInv;
+var velocidadY=0;
+var incrVelY=0.2;
 function preload() {
 
 //CARGAMOS NUESTRAS IMAGENES
   game.load.image('fondo', 'assets/images/fondo.png');
   game.load.spritesheet('pajaro', 'assets/images/flappy_spritesheet_5_1_32_32.png',32,32,5);
+  game.load.image('tuberia', 'assets/images/tuberia.png');
   game.load.image('tuberia', 'assets/images/tuberia.png');
   
 
@@ -23,18 +26,25 @@ function create() {
   fondo = game.add.sprite(0, 0, 'fondo');
   pajaro = game.add.sprite(0, 0, 'pajaro');
   tuberia = game.add.sprite(0, 0, 'tuberia');
+  tuberiaInv = game.add.sprite(0, 0, 'tuberia');
   //ESCALAMOS EL PAJARO
   pajaro.anchor.setTo(0.5);
   pajaro.scale.setTo(2);
   tuberia.anchor.setTo(1,1);
- 
+  
+  tuberiaInv.anchor.setTo(0,1);
+  tuberiaInv.angle=180;
 
   //MOVEMOS EL PAJARO
   pajaro.position.setTo(100,70);
   tuberia.position.setTo(320,640);
+  tuberiaInv.position.setTo(320,0);
 
   //TECLA SPACIO
-  spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+
+  //añadimos la tecnica de tap
+  game.input.onTap.add(onTap,this);
 
   //CREAMOS la animacion del pajaro volando
   pajaroVolando=pajaro.animations.add('pajaroVolando');
@@ -48,41 +58,43 @@ function create() {
 function update(){
 
 
-if(spaceKey.isDown==true){
-  pajaroSube();
-}else{
-  pajaroCae()
-}
-mueveTuberia();
- 
-if(detectaColision(pajaro,tuberia)){
 
-  console.log('COLISION!!!');
-  game.state.restart('flappy');
-  
+    mueveTuberia();
+    pajaroCae();
+    
+    if(detectaColision(pajaro,tuberia)){
+      console.log('COLISION!!!');
+      game.state.restart('flappy');
+    }
+    if(detectaColision(pajaro,tuberiaInv)){
+      console.log('COLISION!!!');
+      game.state.restart('flappy');
+    }
 
-}
 
 }
 
 function mueveTuberia(){
-  tuberia.position.x-=4;
-  if(tuberia.position.x==0){
-    
-    tuberia.position.x=650;
-    
-   } //hemos llegado al final de la pantalla
+      tuberia.position.x-=4;
+      tuberiaInv.position.x-=4;
+      if(tuberia.position.x<=0){
+        
+        tuberia.position.x=650;
+        tuberiaInv.position.x=650
+        
+      } //hemos llegado al final de la pantalla
 }
 
 
 
 
 function pajaroCae(){
-  pajaro.position.y=pajaro.position.y+5;
+  velocidadY=velocidadY+incrVelY;
+  pajaro.position.y=pajaro.position.y+velocidadY;
 }
 
 function pajaroSube(){
-  pajaro.position.y=pajaro.position.y-20;
+  velocidadY=-7;
 }
 
 function detectaColision(spriteA, spriteB) {
@@ -91,5 +103,12 @@ function detectaColision(spriteA, spriteB) {
   var boundsB = spriteB.getBounds();
 
   return Phaser.Rectangle.intersects(boundsA, boundsB);
+
+}
+
+
+function onTap(pointer, doubleTap) {
+
+pajaroSube();  
 
 }
